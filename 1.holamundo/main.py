4a259064d11e3ed2a32477f01e812884f45ec1ac
Lorblak <vasquez97548@gmail.com>
2020-03-29 20:23:17 -0300

    Se agregó Ping Pong horizontal en el repositorio

--- 1.holamundo/main.py
+++ 1.holamundo/main.py
@@ -1,20 +1,19 @@
 import kivy
 kivy.require('1.9.1')
 
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.config import Config
-
+from kivy.app import App 
+from kivy.uix.boxlayout import BoxLayout 
+from kivy.config import Config 
 Config.set('graphics', 'width', 400)
-Config.set('graphics', 'height', 400)
+Config.set('graphics', 'height', 200)
 
-class Box01(BoxLayout):
+class Contenedor_01(BoxLayout):
 	None
 
 class MainApp(App):
 	title = 'Hola Mundo'
 	def build(self):
-		return Box01()
+		return Contenedor_01()
 
 if __name__ == '__main__':
 	MainApp().run()

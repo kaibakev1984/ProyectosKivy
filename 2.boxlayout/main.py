861b9e24afca9aedd155b66175b7f1b1ceaf9a90
Lorblak <vasquez97548@gmail.com>
2020-03-30 00:06:08 -0300

    Se agregaron ejemplos de Stencil y Animation

--- 2.boxlayout/main.py
+++ 2.boxlayout/main.py
@@ -1,15 +1,16 @@
 import kivy
-kivy.require('1.11.1')
+kivy.require('1.9.1')
 
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.app import App 
+from kivy.uix.boxlayout import BoxLayout 
 
 class Box01(BoxLayout):
 	None
 
 class MainApp(App):
-	title = 'BoxLayout Método 1'
+	title = 'BoxLayout Metodo 1'
 	def build(self):
 		return Box01()
+
 if __name__ == '__main__':
 	MainApp().run()
